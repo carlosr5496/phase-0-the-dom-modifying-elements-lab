c08739219171603b765786d133aea39b934238ed
Ryan Carlos <ryancarlos925@gmail.com>
2022-05-23 20:16:23 -0700

    commit tree bbef430705ba71313bd816651e356d24a5438365

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const removeMain = document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'
+document.body.append(newHeader)
+
+
+// const element = document.createElement("h1");
+// element.textContent = 'Hello'
+
